52365a8da2e5ad02d116a50f1a7dd6d17dc96856
Normandes Junior <normandesjr@gmail.com>
2018-05-01 10:28:56 -0300

    Terceira refatoracao

---
+++ terceira-refatoracao/src/main/com/hibicode/EstatisticasDoGoleiro.java
@@ -0,0 +1,49 @@
+package com.hibicode;
+
+import java.util.List;
+
+public class EstatisticasDoGoleiro {
+
+    private Temporada temporada;
+
+    public EstatisticasDoGoleiro(Temporada temporada) {
+        this.temporada = temporada;
+    }
+
+    public double getMediaGolsSofridos() {
+        if (temporada.getJogos().isEmpty()) {
+            return 0.0;
+        } else {
+            List<Jogo> jogos = temporada.getJogos();
+            double mins = 0;
+
+            int tgs = getTotalGolsSofridos(jogos);
+
+            for (Jogo jogo : jogos) {
+                mins += jogo.getMinutosJogados();
+            }
+            return (tgs / mins) * 90;
+        }
+    }
+
+    public double getPercentualDefesa() {
+        if (temporada.getJogos().isEmpty()) {
+            return 0.0;
+        } else {
+            List<Jogo> jogos = temporada.getJogos();
+            int tcg = 0;
+
+            int g = getTotalGolsSofridos(jogos);
+
+            for (Jogo jogo : jogos) {
+                tcg += jogo.getChutesNoGol();
+            }
+            return ((double) tcg - g) / tcg;
+        }
+    }
+
+    private int getTotalGolsSofridos(List<Jogo> jogos) {
+        return jogos.stream().mapToInt(Jogo::getGolsSofridos).sum();
+    }
+
+}
